ee14de97ea599b9311ecdc2e12e1eca0b61742d4
91ko <kjm4121@gmail.com>
2025-10-08 15:28:05 +0900

    Update Naver blog icon to more recognizable blog-style icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -183,7 +183,7 @@ export default function ClinicLanding() {
                  <Button asChild variant="outline" className="border-green-500 text-green-600 hover:bg-green-50">
                    <a href="https://blog.naver.com/sungmo-ieum" target="_blank" rel="noopener noreferrer">
                      <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24" fill="currentColor">
-                       <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+                       <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
                      </svg>
                      <span className="hidden sm:inline">네이버 블로그</span>
                      <span className="sm:hidden">블로그</span>
